a83d89214c3a44999ff5fdbd0544781129520b89
EOussama <oussama_gtacj@live.fr>
2019-12-07 17:37:29 +0100

    Sugar

--- src/conf.lua
+++ src/conf.lua
@@ -6,15 +6,32 @@
 
 ]]
 
+
+
+--[[
+  @description Global configurtions
+  @param {Table} t: Love2D's configuration table
+]]
 function love.conf(t)
-  -- Love2D configuration
-  t.version = '11.0' 
+  
+  -- Specifying the used version of Love2D
+  t.version = '11.0'
+
+  -- Enableling the console
   t.console = true
 
-  -- Game information
+  -- Setting up the window's title
   t.window.title = "Cöin"
+
+  -- Setting up the window's icon
   t.window.icon = "assets/gfx/coin/coin.png"
+
+  -- Setting up the starting display
   t.window.display = 1
+  
+  -- Setting up the screen width
   t.window.width = 800
+
+  -- Setting up the screen height
   t.window.height = 600 
 end
--- src/main.lua
+++ src/main.lua
@@ -6,17 +6,44 @@
 
 ]]
 
+
+
+-- Including the global table for game-wide state tracking
 require "core.utils.global"
+
+-- Including the core of the game
 local Core = require("core.core")
 
+
+
+--[[
+  @description Love2D loading life-cycle event
+]]
 function love.load()
+
+  -- Initializing the game
   Core:init()
 end
 
+
+
+--[[
+  @description Love2D updating life-cycle event
+  @param {Number} dt: Delta time
+]]
 function love.update(dt)
+
+  -- Issuing a game-wide update
   Core:update(dt)
 end
 
+
+
+--[[
+  @description Love2D drawing life-cycle event
+]]
 function love.draw()
+
+  -- Drawing the game
   Core:draw()
 end
