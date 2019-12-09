31ada33bb93ff3bf5b20a9d43040ba3f589005f2
EOussama <oussama_gtacj@live.fr>
2019-12-09 22:10:39 +0100

    Animation exp

--- src/core/entities/player.lua
+++ src/core/entities/player.lua
@@ -29,8 +29,6 @@ local player = {
 function player.init(self)
   self.x = love.graphics.getWidth() / 2 - 25
   self.y = love.graphics.getHeight() / 2 - 25
-
-  player.sprite = UtilAnimation:create(global.assets.gfx.entities.player.down, 0, 0, 50, 128)
 end
 
 
@@ -45,7 +43,6 @@ function player.update(self, dt)
   if love.keyboard.isDown("up") and self.y > global.ui.header.height then
     self.y = self.y - self.velocity
     self.direction = "up"
-    self.sprite:animate()
     
     if self.y < global.ui.header.height then self.y = global.ui.header.height end
   end
@@ -54,7 +51,6 @@ function player.update(self, dt)
   if love.keyboard.isDown("right") and self.x < love.graphics.getWidth() - self.width then
     self.x = self.x + self.velocity
     self.direction = "right"
-    self.sprite:animate()
     
     if self.x > love.graphics.getWidth() then self.x = love.graphics.getWidth() end
   end
@@ -63,7 +59,6 @@ function player.update(self, dt)
   if love.keyboard.isDown("down") and self.y < love.graphics.getHeight() - self.height then
     self.y = self.y + self.velocity
     self.direction = "down"
-    self.sprite:animate()
     
     if self.y > love.graphics.getHeight() then self.y = love.graphics.getHeight() end
   end
@@ -72,7 +67,6 @@ function player.update(self, dt)
   if love.keyboard.isDown("left") and self.x > 0 then
     self.x = self.x - self.velocity
     self.direction = "left"
-    self.sprite:animate()
 
     if self.x < 0 then self.x = 0 end
   end
@@ -88,9 +82,7 @@ function player.draw(self)
 
   -- Drawing the player
   UtilColor.rgba(62, 140, 67, 255, true)
-  -- love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-  love.graphics.draw(global.assets.gfx.entities.player[self.direction], self.x, self.y)
-  
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
   UtilColor.restore()
 end
 
--- src/core/utils/animation.lua
+++ src/core/utils/animation.lua
@@ -13,7 +13,7 @@ local animation = {}
 --[[
   @description Creates an animation table
 ]]
-function animation:create(image, x, y, width, height, speed)
+function animation:create(image, x, y, width, height, max, speed)
   local anim = {
     spriteSheet = image,
     quads = {},
@@ -21,16 +21,33 @@ function animation:create(image, x, y, width, height, speed)
     y = y or 0,
     width = width or 0,
     height = height or 0,
-    speed = speed or 500
+    max = max or 0,
+    speed = speed or 500,
+    currentFrame = 0,
+    tick = 0
   }
 
-  for i = 0, anim.spriteSheet:getHeight() - anim.height, anim.height do
-    for j = 0, anim.spriteSheet:getWidth() - anim.width, anim.width do
-        table.insert(anim.quads, love.graphics.newQuad(i, j, anim.width, anim.height, anim.spriteSheet:getDimensions()))
+  for i = 0, max, anim.width do
+      table.insert(anim.quads, love.graphics.newQuad(i, 0, anim.width, anim.height, anim.spriteSheet:getDimensions()))
+  end
+  
+  function anim:update(dt)
+    self.tick = self.tick + dt
+    
+    -- print(self.tick)
+    if self.tick >= self.speed / 1000 then
+      self.tick = 0
+      self.currentFrame = self.currentFrame + 1
+      
+      if self.currentFrame >= max then
+        self.currentFrame = 0
+      end
     end
   end
 
-  function anim:animate()
+  function anim:getQuad()
+    print(#self.quads, self.currentFrame)
+    return self.quads[self.currentFrame]
   end
 
   return anim
