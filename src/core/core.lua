a5de358e786d62fd7ededcdbc378a208b117dd38
EOussama <oussama_gtacj@live.fr>
2019-12-11 21:18:59 +0100

    Added better doc

--- src/core/core.lua
+++ src/core/core.lua
@@ -16,25 +16,61 @@ local SceneGame = require("core.scenes.game")
 
 local core = {}
 
+
+
+--[[
+  @description Initializes the game
+]]
 function core.init()
+
+  -- Loading the assets
   UtilLoader:loadAssets()
+
+  -- Initializing the game
   UtilScene:init()
 end
 
+
+
+--[[
+  @description Updates the game
+  @param {Table} self: The table that invokes the function call
+  @param {Number} dt: Delta time
+]]
 function core.update(self, dt)
+
+  -- Updating the timer utility
   UtilTimer:update(dt)
+
+  -- Updating the scene utility
   UtilScene:update(dt)
 end
 
+
+
+--[[
+  @description Drawing the game
+]]
 function core.draw()
   UtilScene:draw()
 end
 
+
+
+--[[
+  @description Listens to keyboard inputs
+  @param {String} key: The key that was pressed
+]]
 function love.keypressed(key)
+
+  -- Checking if the 'p' key was pressed
   if key == 'p' then
+
+    -- Toggeling the game's pause status
     global.paused = not global.paused
-    print(global.paused)
   end
 end
+
+
 
 return core
