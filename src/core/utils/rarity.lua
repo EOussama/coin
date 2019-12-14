6374f0e998a3f35097b27bde13ea8072babff810
EOussama <oussama_gtacj@live.fr>
2019-12-14 10:50:58 +0100

    Improved rarity calculation

--- src/core/utils/rarity.lua
+++ src/core/utils/rarity.lua
@@ -22,7 +22,25 @@ function rarity.calculate(rarity)
   -- Sorting the rarity values
   table.sort(rarity)
 
-  print(table.concat(rarity, ' '))
+  for i = 1, #rarity do
+    local j = 1
+    local rarityLevel = {
+      rarity = rarity[i],
+      values = {}
+    }
+
+    while j <= rarityLevel.rarity do
+        rarityLevel.values[j] = math.random(1, 100)
+        j = j + 1
+    end
+
+    table.insert(rarityTable, rarityLevel)
+  end
+
+  print('\n-----\n')
+  for i = 1, #rarityTable do
+    print(rarityTable[i].rarity, table.concat(rarityTable[i].values, ' - '))
+  end
 
   return rarityTable
 end
